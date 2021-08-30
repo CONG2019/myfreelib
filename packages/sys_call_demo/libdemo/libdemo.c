6424653b6bd1f0ce3b67208d0796a5fa0fd5bc6e
cong <1178187720@qq.com>
2021-08-31 01:05:29 +0800

    [sys_call_demo]:
    - Add Makefile for sys_call_demo and modify rule.mk
    - Modify libdemo

--- packages/sys_call_demo/libdemo/libdemo.c
+++ packages/sys_call_demo/libdemo/libdemo.c
@@ -13,6 +13,22 @@ int main(int argc, char const *argv[])
 
 void dldemo(void){
     void *lib_handler;
+    void (*clprogress_print)(const char *, ...);
 
     lib_handler = dlopen(COLORPRINT_LIB_PATH, RTLD_LAZY);
+    if(!lib_handler) printf("Open %s failed!\n", COLORPRINT_LIB_PATH);
+
+    clprogress_print = dlsym(lib_handler, "clprogress_print");
+    if(!clprogress_print) printf("Can not find symbol clprogress_print.\n" );
+
+    clprogress_print("Success.\n");
+
+    dlclose(lib_handler);
 }
+
+int main(int argc, char const *argv[])
+{
+    dldemo();
+    return 0;
+}
+
