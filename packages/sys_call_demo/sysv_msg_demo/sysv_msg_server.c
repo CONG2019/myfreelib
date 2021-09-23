a767add352a023f8687f5342e9a75bc2d6d61998
Jacken Zhang <1178187720@qq.com>
2021-09-23 18:44:35 +0800

    [sysv_msg_demo]: Add signal handler in server to remove message queue when exit.

--- packages/sys_call_demo/sysv_msg_demo/sysv_msg_server.c
+++ packages/sys_call_demo/sysv_msg_demo/sysv_msg_server.c
@@ -6,11 +6,28 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 #include "sysv_msg_demo.h"
 
+static int msgid;
+
+static void sig_handler(int sig){
+	printf("Receive signal(%d).\n", sig);
+	msgctl(msgid, IPC_RMID, NULL);
+	exit(-1);
+}
+
+void clean(void){
+	printf("Remove message queue.\n");
+	msgctl(msgid, IPC_RMID, NULL);
+}
+
 int main(int argc, char *argv[]){
-	int msgid;
 	struct mbuf msg;
+
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+	atexit(clean);
 
 	msgid = msgget(MY_KEY, IPC_CREAT|IPC_EXCL|S_IRUSR|S_IWUSR|S_IRGRP);
 	if(msgid == -1){
