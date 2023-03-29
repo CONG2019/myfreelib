1dbab7dabd561e7addf0fcfbff209d3480111d0d
Jacken <1178187720@qq.com>
2023-03-30 00:18:16 +0800

    [study][cpp] Add cpp firstapp.

---
+++ packages/study/cpp/firstapp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    using namespace std;
+    cout << "Hello world.";
+    cout << endl;
+    printf("Hello world.\n");
+    cin.get();
+    return 0;
+}
